0e32afb8eafc2587d04576efdd704f28f0900eea
luciano093 <luparedes100@gmail.com>
2026-02-12 20:41:08 -0500

    Change uppercase fields to lowercase

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -4,16 +4,16 @@ require_once 'db.php';
 if ($method === 'POST') {
     $data = getRequestData();
     $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login = ?");
-    $stmt->bind_param("s", $data['Login']);
+    $stmt->bind_param("s", $data['login']);
     $stmt->execute();
     $user = $stmt->get_result()->fetch_assoc();
 	
 	// To prevent hashing null values (empty password field)
-    if (!isset($data['Password']) || !isset($data['Login'])) {
+    if (!isset($data['password']) || !isset($data['login'])) {
         sendResponse(["error" => "Login and Password are required"], 400);
     }
 
-    $hash = password_hash($data['Password'], PASSWORD_DEFAULT);
+    $hash = password_hash($data['password'], PASSWORD_DEFAULT);
 
     if ($user && password_verify($hash, $user['Password'])) {
         unset($user['Password']);
