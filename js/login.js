c4cd9ac8924b7522d2fc760f77e753d52054b21a
luciano093 <luparedes100@gmail.com>
2026-02-13 15:00:30 -0500

    Change semicolon to comma in saveCookie function

--- js/login.js
+++ js/login.js
@@ -67,5 +67,5 @@ function saveCookie()
 	let minutes = 20;
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ",expires=" + date.toGMTString();
 }
