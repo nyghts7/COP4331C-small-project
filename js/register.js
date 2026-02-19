56c23ea2a1b2805b7e1d6b00fa74b3cbfee26c76
Aviel Dahaman <avield@gmail.com>
2026-02-19 18:34:58 -0500

    Adding a login after registering to register.js
    Based on TA feedback, the user should be logged in automatically after
    registering.

--- js/register.js
+++ js/register.js
@@ -1,10 +1,13 @@
 const urlBase = 'https://poosdteam13.xyz/LAMPAPI';
 const extension = 'php';
+let userId;
+let firstName;
+let lastName;
 
 function doRegister()
 {
-	let firstName = document.getElementById("firstName").value.trim();
-	let lastName = document.getElementById("lastName").value.trim();
+	let first = document.getElementById("firstName").value.trim();
+	let last = document.getElementById("lastName").value.trim();
 	let login = document.getElementById("username").value.trim();
 	let password = document.getElementById("password").value;
 	let repeatPassword = document.getElementById("verifyPassword").value;
@@ -28,8 +31,8 @@ function doRegister()
 
 	// JSON Payload
 	let tmp = {
-		FirstName: firstName,
-		LastName: lastName,
+		FirstName: first,
+		LastName: last,
 		Login: login,
 		Password: password
 	};
@@ -49,6 +52,7 @@ function doRegister()
 			if(this.status == 201)
 			{
 				result.innerHTML = "User successfully registered!";
+				doLogin(login, password);
 			}
 			else
 			{
@@ -60,4 +64,58 @@ function doRegister()
 
 	xhr.send(jsonPayload);
 
+}
+
+function doLogin(login, password){
+	//Logging in
+	var tmp2 = {login:login,password:password};
+	let jsonPayload2 = JSON.stringify( tmp2 );
+
+	let result = document.getElementById("registerResult");
+
+	let url2 = urlBase + '/login.' + extension;
+
+	let xhr2 = new XMLHttpRequest();
+	xhr2.open("POST", url2, true);
+	xhr2.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr2.onreadystatechange = function() 
+		{
+			if (this.readyState !== 4) return;
+			if (this.status === 200) {
+				let jsonObject = JSON.parse( this.responseText );
+				userId = jsonObject.ID;
+		
+				if( userId < 1 )
+				{		
+					result.innerHTML = "User/Password combination incorrect";
+					return;
+				}
+
+				firstName = jsonObject.FirstName;
+				lastName = jsonObject.LastName;
+
+				saveCookie();
+	
+				window.location.href = "contacts.html";
+			} else if (this.status === 401){
+				result.innerHTML = "User/Password combination incorrect";
+			}
+		};
+		xhr2.send(jsonPayload2);
+	}
+	catch(err)
+	{
+		result.innerHTML = err.message;
+	}
+}
+
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ",expires=" + date.toGMTString();
 }
